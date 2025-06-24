0373544af0bce6df0e465b5d255807c67824b9e1
ishikawa yutaka” <piichanandmukkun@gmail.com>
2025-06-24 13:21:38 +0900

    publicディレクトリへ移動

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
+
+// 環境に応じて設定を変更
+const isDev = process.env.NODE_ENV === "development";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), icon()],
+  site: isDev ? "http://localhost:4323" : "https://your-username.github.io",
+  base: isDev ? "/" : "/studying-astro-portfolio/",
+  integrations: [mdx(), sitemap(), icon()],
 });
